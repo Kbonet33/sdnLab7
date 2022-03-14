747a189df9dfe7cd1eb36adc908652dc01ab9699
Keith Bonet <s354908@students.fscj.edu>
2022-03-13 17:59:09 -0700

    Entered Error Handling Script

--- Connections.py
+++ Connections.py
@@ -1,6 +1,7 @@
 import netmiko
 from netmiko import ConnectHandler
 from getpass import getpass
+from netmiko.ssh_exception import AuthenticationException, SSHException, NetMikoTimeoutException
 
 USERNAME = input("Please enter your SSH username: ")
 PASS = getpass("Please enter your SSH password: ")
@@ -12,11 +13,15 @@ device = {
     'device_type': 'cisco_ios'
 }
 
-c = ConnectHandler(**device)
-
-output = c.send_command('show run')
-
-f = open('backup.conf', 'x')
-
-f.write(output)
-f.close()
+try:
+    c = ConnectHandler(**device)
+    output = c.send_command('show run')
+    f = open('backup.conf', 'x')
+    f.write(output)
+    f.close()
+except (AuthenticationException):
+    print("An Authentication error occured while connecting to: " + device['ip'])
+except (SSHException):
+    print("An error occured while connecting to device " + device['ip'] + "via ssh. Is SSH enabled?")
+except (NetMikoTimeoutException):
+    print("the device " + device['ip'] + " timed out when attempting to connect")
